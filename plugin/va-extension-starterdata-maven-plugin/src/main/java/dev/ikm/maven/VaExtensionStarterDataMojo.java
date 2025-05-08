e09b10936c10711dafc0734c533d836e6a9488df
Joshua Dettinger <joshuadettinger@gmail.com>
2025-05-08 16:03:15 -0500

    IKDT-971 Create VA Extension artifacts (#1)

---
+++ plugin/va-extension-starterdata-maven-plugin/src/main/java/dev/ikm/maven/VaExtensionStarterDataMojo.java
@@ -0,0 +1,93 @@
+package dev.ikm.maven;
+
+import dev.ikm.tinkar.common.service.CachingService;
+import dev.ikm.tinkar.common.service.PrimitiveData;
+import dev.ikm.tinkar.common.service.ServiceKeys;
+import dev.ikm.tinkar.common.service.ServiceProperties;
+import dev.ikm.tinkar.common.util.uuid.UuidT5Generator;
+import dev.ikm.tinkar.composer.Composer;
+import dev.ikm.tinkar.composer.Session;
+import dev.ikm.tinkar.composer.assembler.ConceptAssembler;
+import dev.ikm.tinkar.composer.template.Definition;
+import dev.ikm.tinkar.composer.template.FullyQualifiedName;
+import dev.ikm.tinkar.composer.template.Identifier;
+import dev.ikm.tinkar.composer.template.StatedAxiom;
+import dev.ikm.tinkar.composer.template.Synonym;
+import dev.ikm.tinkar.terms.EntityProxy;
+import dev.ikm.tinkar.terms.State;
+import dev.ikm.tinkar.terms.TinkarTerm;
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+
+import java.io.File;
+import java.util.UUID;
+
+import static dev.ikm.tinkar.terms.TinkarTerm.DESCRIPTION_NOT_CASE_SENSITIVE;
+import static dev.ikm.tinkar.terms.TinkarTerm.ENGLISH_LANGUAGE;
+
+@Mojo(name = "run-va-extension-starterdata", defaultPhase = LifecyclePhase.INSTALL)
+public class VaExtensionStarterDataMojo extends AbstractMojo
+{
+    @Parameter(property = "origin.namespace", required = true)
+    String namespaceString;
+    @Parameter(property = "datastorePath", required = true)
+    private String datastorePath;
+    @Parameter(property = "controllerName", defaultValue = "Open SpinedArrayStore")
+    private String controllerName;
+
+    public void execute() throws MojoExecutionException
+    {
+        try {
+            UUID namespace = UUID.fromString(namespaceString);
+            File datastore = new File(datastorePath);
+
+            CachingService.clearAll();
+            ServiceProperties.set(ServiceKeys.DATA_STORE_ROOT, datastore);
+            PrimitiveData.selectControllerByName(controllerName);
+            PrimitiveData.start();
+
+            Composer composer = new Composer("VA Extension Starter Data Composer");
+
+            Session session = composer.open(State.ACTIVE,
+                    TinkarTerm.USER,
+                    TinkarTerm.PRIMORDIAL_MODULE,
+                    TinkarTerm.PRIMORDIAL_PATH);
+
+            EntityProxy.Concept vaExtensionAuthor = EntityProxy.Concept.make("VA Extension Author", UuidT5Generator.get(namespace, "VA Extension Author"));
+            session.compose((ConceptAssembler concept) -> concept
+                    .concept(vaExtensionAuthor)
+                    .attach((FullyQualifiedName fqn) -> fqn
+                            .language(ENGLISH_LANGUAGE)
+                            .text("VA Extension Author")
+                            .caseSignificance(DESCRIPTION_NOT_CASE_SENSITIVE)
+                    )
+                    .attach((Synonym synonym)-> synonym
+                            .language(ENGLISH_LANGUAGE)
+                            .text("VA Extension Author")
+                            .caseSignificance(DESCRIPTION_NOT_CASE_SENSITIVE)
+                    )
+                    .attach((Definition definition) -> definition
+                            .language(ENGLISH_LANGUAGE)
+                            .text("VA Snomed Extension Author")
+                            .caseSignificance(DESCRIPTION_NOT_CASE_SENSITIVE)
+                    )
+                    .attach((Identifier identifier) -> identifier
+                            .source(TinkarTerm.UNIVERSALLY_UNIQUE_IDENTIFIER)
+                            .identifier(vaExtensionAuthor.asUuidArray()[0].toString())
+                    )
+                    .attach((StatedAxiom statedAxiom) -> statedAxiom
+                            .isA(TinkarTerm.USER)
+                    )
+            );
+
+            composer.commitSession(session);
+            PrimitiveData.stop();
+        } catch (Exception e) {
+            throw new MojoExecutionException("Failed to execute class", e);
+        }
+
+    }
+}
---
+++ plugin/va-extension-transformation-maven-plugin/src/main/java/dev/ikm/maven/AxiomSyntaxTransformer.java
@@ -0,0 +1,79 @@
+package dev.ikm.maven;
+
+import dev.ikm.tinkar.common.id.PublicIds;
+import dev.ikm.tinkar.composer.Composer;
+import dev.ikm.tinkar.composer.Session;
+import dev.ikm.tinkar.composer.template.AxiomSyntax;
+import dev.ikm.tinkar.terms.EntityProxy;
+import dev.ikm.tinkar.terms.State;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.UUID;
+import java.util.stream.Stream;
+
+public class AxiomSyntaxTransformer extends AbstractTransformer {
+
+    private static final Logger LOG = LoggerFactory.getLogger(AxiomSyntaxTransformer.class.getSimpleName());
+    private static final int ID = 0;
+    private static final int EFFECTIVE_TIME = 1;
+    private static final int ACTIVE = 2;
+    private static final int MODULE_ID = 3;
+    private static final int REFSET_ID = 4;
+    private static final int REFERENCED_COMPONENT_ID = 5;
+    private static final int OWL_EXPRESSION = 6;
+    String previousRowId;
+
+    AxiomSyntaxTransformer(UUID namespace) {
+        super(namespace);
+    }
+
+    /**
+     * Parses OWL Expression file and creates Axiom Semantics for each line
+     *
+     * @param axiomFile input file Path
+     */
+    @Override
+    public void transform(File axiomFile, Composer composer) {
+        EntityProxy.Concept author = VaExtensionUtility.getUserConcept(namespace);
+        EntityProxy.Concept path = VaExtensionUtility.getPathConcept();
+
+        try (Stream<String> lines = Files.lines(axiomFile.toPath())) {
+            lines.skip(1)
+                    .map(row -> row.split("\t"))
+                    .forEach(data -> {
+                        State status = data[ACTIVE].equals("1") ? State.ACTIVE : State.INACTIVE;
+                        long time = VaExtensionUtility.timestampToEpochSeconds(data[EFFECTIVE_TIME]);
+                        EntityProxy.Concept module = EntityProxy.Concept.make(PublicIds.of(VaExtensionUtility.generateUUID(namespace, data[MODULE_ID])));
+
+                        Session session = composer.open(status, time, author, module, path);
+                        configureSemanticsForConcept(session, data);
+                    });
+        } catch(IOException | SecurityException ex) {
+            LOG.info(ex.toString());
+        }
+    }
+
+    /**
+     * Checks previous row for duplicate id and creates a version instead of a new semantic
+     *
+     * @param session initialized Session from the Composer API
+     * @param columns line from OWL expression file split on tabs
+     */
+    private void configureSemanticsForConcept(Session session, String[] columns) {
+        String owlExpressionWithPublicIds = VaExtensionUtility.owlAxiomIdsToPublicIds(namespace, columns[OWL_EXPRESSION]);
+
+        EntityProxy.Concept concept = EntityProxy.Concept.make(PublicIds.of(VaExtensionUtility.generateUUID(namespace, columns[REFERENCED_COMPONENT_ID])));
+        EntityProxy.Semantic axiomSemantic = EntityProxy.Semantic.make(PublicIds.of(VaExtensionUtility.generateUUID(namespace, columns[ID])));
+        previousRowId = columns[ID];
+
+        session.compose(new AxiomSyntax()
+                        .semantic(axiomSemantic)
+                        .text(owlExpressionWithPublicIds),
+                concept);
+    }
+
+}
